e4352f6a3b5c22be409318746207e96dd429e0cd
Lukas Karberg <deaderlp@googlemail.com>
2024-01-30 11:50:09 +0100

    Refactor product list layout and styling

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,3 @@
-import { Nav } from "@/components/Nav";
 import db from "@/data/db.json";
 
 const products = db.products;
@@ -7,11 +6,26 @@ export default function Home() {
   return (
     <main>
       <h2 className="text-2xl mb-2 font-bold">Products</h2>
-      <ul>
+      <ul className="flex flex-col gap-4">
         {products.map((product) => {
           return (
-            <div key={product.id}>
-              {product.model} / {product.category}
+            <div
+              key={product.id}
+              className="bg-slate-600 p-4 rounded-md w-1/2 flex"
+            >
+              <p className="font-bold text-2xl text-green-300">{product.id}</p>
+              <img
+                className="mx-3"
+                src="images/products/mock.jpg"
+                alt="a simple contentless mock up picture"
+              />
+              <section>
+                <p className="text-yellow-400">{product.category}</p>
+                <p className="font-semibold text-2xl">
+                  {product.brand} {product.model}
+                </p>
+                <p className="text-red-400 font-bold">{product.price} €</p>
+              </section>
             </div>
           );
         })}
